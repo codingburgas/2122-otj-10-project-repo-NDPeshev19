aa7ae49668c18e0236e1e182e7e769656ef04d69
NDPeshev19 <NDPeshev19@codingburgas.bg>
2022-07-04 11:28:54 +0300

    Add userManager submenu

--- src/pm.pl/include/cli.h
+++ src/pm.pl/include/cli.h
@@ -10,4 +10,6 @@ namespace pm::pl
 {
 	std::unique_ptr<cli::Menu> getMenu(bool isAdmin);
 	void cli(std::unique_ptr<cli::Menu> menu);
+	
+	std::unique_ptr<cli::Menu> getUserManagerSubMenu(bool isAdmin);
 }
--- src/pm.pl/src/cli.cpp
+++ src/pm.pl/src/cli.cpp
@@ -2,13 +2,14 @@
 
 std::unique_ptr<cli::Menu> pm::pl::getMenu(bool isAdmin)
 {
-	auto rootMenu = 
-		std::make_unique<cli::Menu>("root");
+	auto rootMenu =  std::make_unique<cli::Menu>("main");
 	
 	rootMenu->Insert("ping", [](std::ostream& out) 
 	{
 		std::cout << "pong" << '\n'; 
 	});
+
+	rootMenu->Insert(std::move(pm::pl::getUserManagerSubMenu(isAdmin)));
 
 	return rootMenu;
 }
@@ -39,4 +40,16 @@ void pm::pl::cli(std::unique_ptr<cli::Menu> menu)
 	scheduler.Post([] {std::cout << "Welcome to the Project Manager!\n"; });
 	
 	scheduler.Run();
+}
+
+std::unique_ptr<cli::Menu> pm::pl::getUserManagerSubMenu(bool isAdmin)
+{
+	auto menu = std::make_unique<cli::Menu>("userManager");
+
+	menu->Insert("ping", [](std::ostream& out)
+		{
+			std::cout << "User pong\n";
+		});
+
+	return menu;
 }
